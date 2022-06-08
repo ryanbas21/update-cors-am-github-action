2aa2ec8e9bfb9eb84a9804aac496fa99305e427f
Ryan Bas <ryan.basmajian@forgerock.com>
2022-06-08 14:30:55 -0600

    init commit

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'lib-update-cors-cloud',
+  setupFiles: ['./test/setup-env.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/lib/update-cors-cloud',
+};
